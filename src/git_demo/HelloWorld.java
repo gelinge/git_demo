5ee90e934e09c6d8eb1505328aaa5990b459d4e8
gelinge <didi7758@qq.com>
2016-07-28 16:17:35 +0900

    删除

--- src/git_demo/HelloWorld.java
+++ src/git_demo/HelloWorld.java
@@ -3,8 +3,5 @@ package git_demo;
 public class HelloWorld {
 	public static void main(String args[]){
 		System.out.println("Hello!World");
-		System.out.println("JAVA!");
-		System.out.println("JAVA!");
-		System.out.println("JAVA!");
 	}
 }
