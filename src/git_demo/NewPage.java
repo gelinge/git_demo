e7189a932f23abbeadb2ba00400a93896780165e
green <didi7758@qq.com>
2016-07-28 16:34:47 +0900

    Update NewPage.java

--- src/git_demo/NewPage.java
+++ src/git_demo/NewPage.java
@@ -3,7 +3,5 @@ package git_demo;
 public class NewPage {
 	public static void main(String args[]){
 		System.out.println("Hello!World");
-		System.out.println("Hello!World");
-		System.out.println("Hello!World");
 	}
 }
